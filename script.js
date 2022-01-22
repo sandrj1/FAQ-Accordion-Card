8b2bcb7d9b0140e9bcee114576d17bb0611f03ef
Sandra Bote <sandra.bote98@gmail.com>
2022-01-22 19:48:54 +0100

    mobile & desktop version done

---
+++ script.js
@@ -0,0 +1,52 @@
+var body = document.getElementsByTagName("body")[0];
+var boxes = document.getElementsByClassName("box");
+var accordion = document.getElementsByClassName("box__question");
+var i;
+var hideAnswer = true;
+
+function toggleAnswer() {
+    var question = this.firstChild;
+    var arrow = this.lastChild;
+    var answer = this.nextElementSibling;
+    var j;
+    hideAnswer = false;
+
+    function openBox() {
+        question.style.fontWeight = "var(--fw-bold)";
+        arrow.style.transform = "rotateX(180deg)";
+        answer.style.display = "unset";
+        hideAnswer = true;
+    }
+
+    function closeBox() {
+        question.style.fontWeight = "unset";
+        arrow.style.transform = "rotateX(0deg)";
+        answer.style.display = "none";
+        hideAnswer = false;
+    }
+
+    for (j = 0; j < accordion.length; j++){
+        if (accordion[j].nextElementSibling.style.display === "unset") {
+            accordion[j].firstChild.style.fontWeight = "unset";
+            accordion[j].lastChild.style.transform = "rotateX(0deg)";
+            accordion[j].nextElementSibling.style.display = "none";
+            if (hideAnswer) {
+                openBox();
+            } else {
+                closeBox();
+            }
+            
+        } else if (accordion[j].nextElementSibling === answer) {
+            if (!hideAnswer) {
+                openBox();
+            } else {
+                closeBox();
+            }
+        } 
+    }
+    
+}
+
+for (i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener("click", toggleAnswer);
+}
